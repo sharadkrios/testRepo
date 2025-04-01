ee94e7634f6f6afea16925a372ce18ca3cbf2314
sharadkrios <sharad.kamble@kriosispl.com>
2025-04-01 10:28:15 +0530

    first commit

--- src/main/java/com/ciranet/myworkqueue/testcases/ProjectsTasksPageTest.java
+++ src/main/java/com/ciranet/myworkqueue/testcases/ProjectsTasksPageTest.java
@@ -24,7 +24,7 @@ public class ProjectsTasksPageTest extends TestBase
 		TestBase.setExtentReportSettings("Projects / Tasks", Constants.SMOKE_TESTING,
 				"Verify Projects / Tasks", "Verifying Projects / Tasks navigation");
 		projectsTasksPage = new ProjectsTasksPage(driver);
-		assertTrue(projectsTasksPage.navigateToProjectsTasks(), "Error navigating to Projects / Tasks Page");
+		assertTrue(projectsTasksPage.navigateToProjectsTasks());
 	}
 
 	@Test(priority = 1, description = "Verify tab Switching", groups = { Constants.FUNCTIONAL_TESTING })
@@ -34,7 +34,7 @@ public class ProjectsTasksPageTest extends TestBase
 		TestBase.setExtentReportSettings("Tab Switch", Constants.FUNCTIONAL_TESTING, "Verify Tab Switch",
 				"Verifying Tab Switch");
 		projectsTasksPage = new ProjectsTasksPage(driver);
-		assertTrue(projectsTasksPage.verifyTabSwitching(), "Error Tab switching is not working");
+		assertTrue(projectsTasksPage.verifyTabSwitching());
 	}
 
 	@Test(priority = 2, description = "Verify Refresh Button", groups = { Constants.FUNCTIONAL_TESTING })
@@ -43,7 +43,7 @@ public class ProjectsTasksPageTest extends TestBase
 		TestBase.setExtentReportSettings("Refresh Button", Constants.FUNCTIONAL_TESTING,
 				"Verify Refresh button", "Verifying Refresh button");
 		projectsTasksPage = new ProjectsTasksPage(driver);
-		assertTrue(projectsTasksPage.verifyRefreshButtonClick(), "Error while Refresh Button is not visible");
+		assertTrue(projectsTasksPage.verifyRefreshButtonClick());
 	}
 
 	@Test(priority = 3, description = "Verify Notification Settings", groups = { Constants.FUNCTIONAL_TESTING })
@@ -52,8 +52,7 @@ public class ProjectsTasksPageTest extends TestBase
 		TestBase.setExtentReportSettings("Notification Settings", Constants.FUNCTIONAL_TESTING,
 				"Verify Notification Settings", "Verifying Notification Settings");
 		projectsTasksPage = new ProjectsTasksPage(driver);
-		assertTrue(projectsTasksPage.verifyNotificationSettingsButton(),
-				"Error 'Notification Settings' button is not visible");
+		assertTrue(projectsTasksPage.verifyNotificationSettingsButton());
 	}
 
 	@Test(priority = 4, description = "Verify Expand And Collapse icon", groups = { Constants.FUNCTIONAL_TESTING })
@@ -62,8 +61,7 @@ public class ProjectsTasksPageTest extends TestBase
 		TestBase.setExtentReportSettings("Expand Collapse icon", Constants.FUNCTIONAL_TESTING, "Verify Expand Collapse icon",
 				"Verifying Expand Collapse icon");
 		projectsTasksPage = new ProjectsTasksPage(driver);
-		assertTrue(projectsTasksPage.verifyToolbarExpandAndCollapse(),
-				"Error 'Expand/Collapse' icon is not visible");
+		assertTrue(projectsTasksPage.verifyToolbarExpandAndCollapse());
 	}
 
 	@Test(priority = 5, description = "Verify Full screen icon", groups = { Constants.FUNCTIONAL_TESTING })
@@ -72,7 +70,7 @@ public class ProjectsTasksPageTest extends TestBase
 		TestBase.setExtentReportSettings("Full Screen", Constants.FUNCTIONAL_TESTING, "Verify Full Screen icon",
 				"Verifying Full Screen icon");
 		projectsTasksPage = new ProjectsTasksPage(driver);
-		assertTrue(projectsTasksPage.verifyToolbarFullScreen(), "Error 'Full Screen' icon is not visible");
+		assertTrue(projectsTasksPage.verifyToolbarFullScreen());
 	}
 
 	@Test(priority = 6, description = "Verify Normal screen icon", groups = { Constants.FUNCTIONAL_TESTING })
@@ -82,8 +80,7 @@ public class ProjectsTasksPageTest extends TestBase
 		TestBase.setExtentReportSettings("Normal Screen Icon", Constants.FUNCTIONAL_TESTING, "Verify Normal Screen",
 				"Verifying Normal screen icon");
 		projectsTasksPage = new ProjectsTasksPage(driver);
-		assertTrue(projectsTasksPage.verifyToolbarNormalScreen(),
-				"Error 'Normal Screen' icon is not visible after entering full-screen mode");
+		assertTrue(projectsTasksPage.verifyToolbarNormalScreen());
 	}
 
 	@Test(priority = 7, description = "Verify Column Chooser icon", groups = { Constants.FUNCTIONAL_TESTING })
@@ -92,8 +89,7 @@ public class ProjectsTasksPageTest extends TestBase
 		TestBase.setExtentReportSettings("Column Chooser icon", Constants.FUNCTIONAL_TESTING,
 				"Verify Column Chooser icon", "Verifying Column chooser icon");
 		projectsTasksPage = new ProjectsTasksPage(driver);
-		assertTrue(projectsTasksPage.verifyToolbarColumnChooserIcon(),
-				"Error 'Column Chooser' icon is not visible");
+		assertTrue(projectsTasksPage.verifyToolbarColumnChooserIcon());
 	}
 
 	@Test(priority = 8, description = "Verify Add New Project Task", groups = { Constants.FUNCTIONAL_TESTING })
@@ -102,8 +98,7 @@ public class ProjectsTasksPageTest extends TestBase
 		TestBase.setExtentReportSettings("Add New Project Task", Constants.FUNCTIONAL_TESTING,
 				"Verify Add New Project task", "Verifying Add New Project Task");
 		projectsTasksPage = new ProjectsTasksPage(driver);
-		assertTrue(projectsTasksPage.verifyAddNewProjectTaskButton(),
-				"Error 'Add New Project Task' button is not clickable");
+		assertTrue(projectsTasksPage.verifyAddNewProjectTaskButton());
 	}
 
 	@Test(priority = 9, description = "Verify Closed within Days Textbox", groups = { Constants.FUNCTIONAL_TESTING })
@@ -112,8 +107,7 @@ public class ProjectsTasksPageTest extends TestBase
 		TestBase.setExtentReportSettings("Closed within Days Textbox", Constants.FUNCTIONAL_TESTING,
 				"Verify Closed within Days Textbox", "Verifying Closed within Days Textbox");
 		projectsTasksPage = new ProjectsTasksPage(driver);
-		assertTrue(projectsTasksPage.verifyClosedWithinDaysTextBox(),
-				"Error 'Closed within Days' textbox is not clickable");
+		assertTrue(projectsTasksPage.verifyClosedWithinDaysTextBox());
 	}
 
 	@Test(priority = 10, description = "Verify Action View/ Edit", groups = { Constants.FUNCTIONAL_TESTING })
@@ -123,7 +117,7 @@ public class ProjectsTasksPageTest extends TestBase
 		TestBase.setExtentReportSettings("Action View/ Edit", Constants.FUNCTIONAL_TESTING, "Verify Action View/ Edit",
 				"Verifying Action View/ Edit");
 		projectsTasksPage = new ProjectsTasksPage(driver);
-		assertTrue(projectsTasksPage.verifyActionViewEdit(), "Error 'Action View/ Edit' button is not clickable");
+		assertTrue(projectsTasksPage.verifyActionViewEdit());
 	}
 
 	@Test(priority = 11, description = "Verify Help icon", groups = { Constants.FUNCTIONAL_TESTING })
@@ -133,7 +127,7 @@ public class ProjectsTasksPageTest extends TestBase
 		TestBase.setExtentReportSettings("Help icon", Constants.FUNCTIONAL_TESTING, "Verify Help icon",
 				"Verifying Help icon");
 		projectsTasksPage = new ProjectsTasksPage(driver);
-		assertTrue(projectsTasksPage.verifyHelpIcon(), "Error 'Help' is not visible");
+		assertTrue(projectsTasksPage.verifyHelpIcon());
 	}
 
 }
